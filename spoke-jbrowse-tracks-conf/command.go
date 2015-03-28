04fc1205397491da9906c4c08b8ea15c84e2e3ad
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2015-03-27 19:54:33 -0500

    Added nowait option for copy command Added watchcopy command

--- spoke-jbrowse-tracks-conf/command.go
+++ spoke-jbrowse-tracks-conf/command.go
@@ -12,6 +12,7 @@ import (
 	zmq "github.com/pebbe/zmq4"
 
 	"gopkg.in/codegangsta/cli.v1"
+	"gopkg.in/fsnotify.v1"
 	"gopkg.in/yaml.v2"
 )
 
@@ -41,6 +42,10 @@ func main() {
 					Usage: "IP address to receive the completion of name indexing",
 					Value: "name:9996",
 				},
+				cli.BoolFlag{
+					Name:  "nowait",
+					Usage: "Do not wait to receive message for indexing",
+				},
 			},
 		},
 		{
@@ -53,6 +58,25 @@ func main() {
 					Usage:  "Yaml configuration file for removing trackList.json",
 					EnvVar: "TRACKS_CONF",
 					Value:  "/config/jbrowse/tracks.yml",
+				},
+			},
+		},
+		{
+			Name:   "watchcopy",
+			Usage:  "Watches and copy config files from jbrowse folder",
+			Action: WatchCopyAction,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "config-file",
+					Usage:  "Yaml configuration file for copying tracks.conf",
+					EnvVar: "TRACKS_CONF",
+					Value:  "/config/jbrowse/tracks.yml",
+				},
+				cli.StringFlag{
+					Name:   "config-folder",
+					Usage:  "Config folder to watch",
+					Value:  "/config/jbrowse",
+					EnvVar: "TRACKS_FOLDER",
 				},
 			},
 		},
@@ -91,27 +115,12 @@ func CopyAction(c *cli.Context) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Wait for the completion before start the copying
-	ReceiveMsg(c.String("receive-name"), "name:complete")
-	// copy all the tracks configuration files
-	for _, cs := range tconf.Config {
-		cl := strings.Split(cs, "=")
-		r, err := os.Open(cl[0])
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer r.Close()
-		w, err := os.Create(filepath.Join(cl[1], "tracks.conf"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer w.Close()
-		_, err = io.Copy(w, r)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("copied %s file to %s", cl[0], cl[1])
+	if !c.Bool("nowait") {
+		// Wait for the completion before start the copying
+		ReceiveMsg(c.String("receive-name"), "name:complete")
 	}
+	// copy all the tracks configuration files
+	CopyTracksConfig(tconf)
 }
 
 func ReceiveMsg(receive string, msg string) {
@@ -136,4 +145,85 @@ func ReceiveMsg(receive string, msg string) {
 	}
 	log.Fatalf("did not receive import message %s\n", msg)
 
+}
+
+func WatchCopyAction(c *cli.Context) {
+	b, err := ioutil.ReadFile(c.String("config-file"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var tconf TracksConf
+	err = yaml.Unmarshal(b, &tconf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmap := make(map[string]string)
+	// create map of file name
+	for _, cs := range tconf.Config {
+		cl := strings.Split(cs, "=")
+		cmap[cl[0]] = cl[1]
+	}
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Printf("Modified file %s", event.Name)
+					if v, ok := cmap[event.Name]; ok {
+						r, err := os.Open(event.Name)
+						if err != nil {
+							log.Fatal(err)
+						}
+						defer r.Close()
+						w, err := os.Create(filepath.Join(v, "tracks.conf"))
+						if err != nil {
+							log.Fatal(err)
+						}
+						defer w.Close()
+						_, err = io.Copy(w, r)
+						if err != nil {
+							log.Fatal(err)
+						}
+						log.Printf("copied %s file to %s", event.Name, v)
+					}
+				}
+			case err := <-watcher.Errors:
+				log.Println(err)
+			}
+		}
+
+	}()
+	log.Printf("Going to watch %s", c.String("config-folder"))
+	err = watcher.Add(c.String("config-folder"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
+
+func CopyTracksConfig(tconf TracksConf) {
+	for _, cs := range tconf.Config {
+		cl := strings.Split(cs, "=")
+		r, err := os.Open(cl[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer r.Close()
+		w, err := os.Create(filepath.Join(cl[1], "tracks.conf"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer w.Close()
+		_, err = io.Copy(w, r)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("copied %s file to %s", cl[0], cl[1])
+	}
 }
